d1832718ab3cba7fa0ec35976b9464d23db555d3
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-09-04 18:26:37 +0800

    Add files via upload

---
+++ src/main/java/org/jmin/util/concurrent/synchronizer/ResourceSynchronizer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright(C) Chris2018998
+ *
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.jmin.util.concurrent.synchronizer;
+
+import org.jmin.util.concurrent.synchronizer.chain.SynchronizeNodeChain;
+
+/**
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+public interface ResourceSynchronizer extends SynchronizeNodeChain {
+
+    //***************************************************************************************************************//
+    //                                           1: resource state/size                                              //
+    //***************************************************************************************************************//
+    int getState();
+
+    void setState(int newState);
+
+    boolean compareAndSetState(int expect, int update);
+
+    //***************************************************************************************************************//
+    //                                           2: resource acquire  methods                                        //
+    //***************************************************************************************************************//
+    boolean isFair();
+
+    boolean tryAcquire(long timeoutNs);
+
+    boolean tryRelease();
+
+    boolean tryAcquireShared(long timeoutNs);
+
+    boolean tryReleaseShared();
+
+    boolean isHeldExclusively();
+
+}
