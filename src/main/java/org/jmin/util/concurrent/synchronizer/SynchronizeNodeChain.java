aa837152b9ac25a0a5ccb48597b2f02358524a39
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-09-06 22:37:02 +0800

    Add files via upload

---
+++ src/main/java/org/jmin/util/concurrent/synchronizer/SynchronizeNodeChain.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright(C) Chris2018998
+ *
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.jmin.util.concurrent.synchronizer;
+
+/**
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+public interface SynchronizeNodeChain {
+
+    int getLength();
+
+    int getLength(int state);
+
+    SynchronizeNode[] getNodes();
+
+    SynchronizeNode[] getNodes(int state);
+
+    Thread[] getNodeThreads();
+
+    Thread[] getNodeThreads(int state);
+
+    SynchronizeNode addNode(int state);
+
+    void removeNode(SynchronizeNode node);
+
+}
+
+
+
+
---
+++ src/main/java/org/jmin/util/concurrent/synchronizer/SynchronizeResource.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright(C) Chris2018998
+ *
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.jmin.util.concurrent.synchronizer;
+
+/**
+ * @author Chris Liao
+ * @version 1.0
+ * <p>
+ * <p>
+ * base
+ */
+public interface SynchronizeResource extends SynchronizeNodeChain {
+
+    boolean isFair();
+
+    boolean tryAcquire(long timeoutNs);
+
+    boolean tryRelease();
+
+}
