2705902f2bad340a38232eb4f5b2baacb182abe5
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-09-08 07:56:44 +0800

    Add files via upload

---
+++ src/main/java/org/jmin/util/concurrent/synchronizer/PermitPool.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright(C) Chris2018998
+ *
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.jmin.util.concurrent.synchronizer;
+
+import org.jmin.util.concurrent.synchronizer.chain.SynchronizeNodeChain;
+
+/**
+ * @author Chris Liao
+ * @version 1.0
+ * <p>
+ * base
+ */
+public interface PermitPool extends SynchronizeNodeChain {
+
+    boolean isFair();
+
+    boolean tryRelease(int args);
+
+    boolean tryAcquireUnInterruptibly(int args, long timeoutNs);
+
+    boolean tryAcquireInterruptibly(int args, long timeoutNs) throws InterruptedException;
+}
---
+++ src/main/java/org/jmin/util/concurrent/synchronizer/ReusePermitPool.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright(C) Chris2018998
+ *
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.jmin.util.concurrent.synchronizer;
+
+/**
+ * @author Chris Liao
+ * @version 1.0
+ * <p>
+ * for semaphore
+ */
+
+public interface ReusePermitPool extends PermitPool {
+
+    int getSize();
+
+    int getMaxSize();
+
+}
