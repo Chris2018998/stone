80259cae5a66c5359009dd47e32747937b9e2030
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-09-12 18:26:19 +0800

    Add files via upload

---
+++ src/main/java/org/jmin/util/atomic/IntegerFieldUpdaterImpl.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright(C) Chris2018998
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.jmin.util.atomic;
+
+import org.jmin.util.UnsafeUtil;
+import sun.misc.Unsafe;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+/**
+ * Atomic Integer Field Updater Implementation(Don't use in other place)
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public final class IntegerFieldUpdaterImpl<T> extends AtomicIntegerFieldUpdater<T> {
+    private final static Unsafe unsafe = UnsafeUtil.getUnsafe();
+    private final long offset;
+
+    private IntegerFieldUpdaterImpl(long offset) {
+        this.offset = offset;
+    }
+
+    public static <T> AtomicIntegerFieldUpdater<T> newUpdater(Class<T> beanClass, String fieldName) {
+        try {
+            return new IntegerFieldUpdaterImpl<T>(unsafe.objectFieldOffset(beanClass.getDeclaredField(fieldName)));
+        } catch (Throwable e) {
+            return AtomicIntegerFieldUpdater.newUpdater(beanClass, fieldName);
+        }
+    }
+
+    public final boolean compareAndSet(T bean, int expect, int update) {
+        return unsafe.compareAndSwapInt(bean, this.offset, expect, update);
+    }
+
+    public final boolean weakCompareAndSet(T bean, int expect, int update) {
+        return unsafe.compareAndSwapInt(bean, this.offset, expect, update);
+    }
+
+    public final void set(T bean, int newValue) {
+        unsafe.putIntVolatile(bean, this.offset, newValue);
+    }
+
+    public final void lazySet(T bean, int newValue) {
+        unsafe.putOrderedInt(bean, this.offset, newValue);
+    }
+
+    public final int get(T bean) {
+        return unsafe.getIntVolatile(bean, this.offset);
+    }
+
+    /**
+     * Atomic Reference Field Updater Implementation(Don't use in other place)
+     *
+     * @author Chris Liao
+     * @version 1.0
+     */
+    public static final class AtomicReferenceFieldUpdaterImpl<T, V> extends AtomicReferenceFieldUpdater<T, V> {
+        private final static Unsafe unsafe = UnsafeUtil.getUnsafe();
+        private final long offset;
+        private final Class<V> fieldType;
+
+        private AtomicReferenceFieldUpdaterImpl(long offset, Class<V> fieldType) {
+            this.offset = offset;
+            this.fieldType = fieldType;
+        }
+
+        public static <T, V> AtomicReferenceFieldUpdater<T, V> newUpdater(Class<T> beanClass, Class<V> fieldType, String fieldName) {
+            try {
+                return new AtomicReferenceFieldUpdaterImpl<T, V>(unsafe.objectFieldOffset(beanClass.getDeclaredField(fieldName)), fieldType);
+            } catch (Throwable e) {
+                return AtomicReferenceFieldUpdater.newUpdater(beanClass, fieldType, fieldName);
+            }
+        }
+
+        public final boolean compareAndSet(T bean, V expect, V update) {
+            return unsafe.compareAndSwapObject(bean, this.offset, expect, update);
+        }
+
+        public final boolean weakCompareAndSet(T bean, V expect, V update) {
+            return unsafe.compareAndSwapObject(bean, this.offset, expect, update);
+        }
+
+        public final void set(T bean, V newValue) {
+            unsafe.putObjectVolatile(bean, this.offset, newValue);
+        }
+
+        public final void lazySet(T bean, V newValue) {
+            unsafe.putOrderedObject(bean, this.offset, newValue);
+        }
+
+        public final V get(T bean) {
+            return fieldType.cast(unsafe.getObjectVolatile(bean, this.offset));
+        }
+    }
+}
---
+++ src/main/java/org/jmin/util/queue/MyCountDownLatch.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright Chris2018998
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmin.util.queue;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * RunnableFuture implementation
+ *
+ * @author Chris Liao
+ */
+
+public class MyCountDownLatch {
+    //waiter status
+    private static final int WAITING = 0;//LockSupport.park()
+    private static final int WAKEUP = 1;//LockSupport.unpark()
+    private static final int FAILED = 2;//interrupt or timeout
+    private static final AtomicIntegerFieldUpdater<Waiter> updater = AtomicIntegerFieldUpdater
+            .newUpdater(Waiter.class, "state");
+    private AtomicInteger atomicCount;
+    private ConcurrentLinkedQueue<Waiter> waitQueue;
+
+    public MyCountDownLatch(int count) {
+        if (count < 0) throw new IllegalArgumentException("count < 0");
+        this.atomicCount = new AtomicInteger(count);
+        waitQueue = new ConcurrentLinkedQueue<Waiter>();
+    }
+
+    public long getCount() {
+        return atomicCount.get();
+    }
+
+    public void countDown() {
+        int c;
+        do {
+            c = atomicCount.get();
+            if (c == 0) return;
+            if (atomicCount.compareAndSet(c, c - 1)) {
+                if (c == 1) break;
+                else return;
+            }
+        } while (true);
+
+        //wakeup
+        Waiter waiter;
+        while ((waiter = waitQueue.poll()) != null) {
+            if (updater.compareAndSet(waiter, WAITING, WAKEUP))
+                LockSupport.unpark(waiter.thread);
+        }
+    }
+
+    public void await() throws InterruptedException {
+        await(0, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean await(long timeout, TimeUnit unit) throws InterruptedException {
+        if (atomicCount.get() > 0) {
+            Waiter waiter = new Waiter();
+            waitQueue.add(waiter);
+            LockSupport.park(unit.toNanos(timeout));
+            if (waiter.state == WAITING && updater.compareAndSet(waiter, WAITING, FAILED)) {
+                waitQueue.remove(waiter);
+                if (waiter.thread.isInterrupted())
+                    throw new InterruptedException();
+            }
+        }
+
+        return atomicCount.get() == 0;
+    }
+
+    private static final class Waiter {
+        volatile int state;
+        Thread thread = Thread.currentThread();
+    }
+}
