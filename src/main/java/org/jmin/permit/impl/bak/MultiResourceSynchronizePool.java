b390b28392a8b4a6458027a54b82fbcfef62d62a
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-09-12 18:19:50 +0800

    Add files via upload

---
+++ src/main/java/org/jmin/permit/impl/bak/MultiResourceSynchronizePool.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright(C) Chris2018998
+ *
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.jmin.permit.impl.bak;
+
+/**
+ * Multi resource synchronize pool
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+public class MultiResourceSynchronizePool extends BaseResourceSynchronizePool {
+
+    //***************************************************************************************************************//
+    //                                          1: Constructors                                                      //
+    //***************************************************************************************************************//
+
+    public MultiResourceSynchronizePool(int resourceSize) {
+        super(resourceSize);
+    }
+
+    public MultiResourceSynchronizePool(int resourceSize, boolean fair) {
+        super(resourceSize, fair);
+    }
+}
