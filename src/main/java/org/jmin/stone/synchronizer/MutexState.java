5727535e54256479f4cf520e1358c98c4c3068d0
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-09-17 22:41:31 +0800

    Add files via upload

---
+++ src/main/java/org/jmin/stone/synchronizer/MutexState.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright(C) Chris2018998
+ *
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.jmin.stone.synchronizer;
+
+/**
+ * @author Chris Liao
+ * @version 1.0
+ * <p>
+ * design for Lock(ReentrantLock/ReentrantReadWriteLock)
+ */
+public interface MutexState {
+
+    boolean isFair();
+
+    //current state
+    int getState();
+
+    //try to acquire a permit,if interrupted,then throws InterruptedException
+    void release(int state);
+
+    //try to acquire a permit,if interrupted,then ignore it and continue to try
+    boolean acquireUninterruptibly(int state, long deadlineNs);
+
+    //try to acquire a permit,if interrupted,then throws InterruptedException
+    boolean acquire(int state, long deadlineNs) throws InterruptedException;
+
+}
