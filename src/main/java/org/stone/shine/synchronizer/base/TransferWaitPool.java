db0edc4f1e956139c90e8270c9c6ba4a007c900a
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-04 21:41:17 +0800

    Add files via upload

--- src/main/java/org/stone/shine/synchronizer/base/TransferWaitPool.java
+++ src/main/java/org/stone/shine/synchronizer/base/TransferWaitPool.java
@@ -9,7 +9,7 @@
  */
 package org.stone.shine.synchronizer.base;
 
-import org.stone.shine.synchronizer.ThreadWaitPool;
+import org.stone.shine.synchronizer.*;
 
 /**
  * transfer wait pool
@@ -19,5 +19,40 @@ import org.stone.shine.synchronizer.ThreadWaitPool;
  */
 public class TransferWaitPool extends ThreadWaitPool {
 
+    //transfer a object to waiter
+    public final boolean tryTransfer(Object object) {
+        return super.wakeupOneToState(object) == 1;
+    }
 
+    /**
+     * get an transferred object from pool
+     *
+     * @param throwsIE true,throws InterruptedException when interrupted
+     * @return boolean value,true means wakeup with expect signal state,false,wait timeout
+     * @throws InterruptedException throw it when throwsIE parameter is true and thread interrupted
+     */
+    public final Object get(ThreadParkSupport support, boolean throwsIE) throws InterruptedException {
+        //1:create wait node and offer to wait queue
+        ThreadNode node = super.appendNewNode();
+
+        //2:spin control
+        try {
+            do {
+                //2.1:read state
+                Object state = node.getState();//any not null value regard as wakeup signal
+                if (state != null) return state;
+
+                //here:null state
+                if (support.getParkTime() <= 0) {//timeout
+                    //two types(1:null state 2:invalid state,not expected state)
+                    if (ThreadNodeUpdater.casNodeState(node, null, ThreadNodeState.TIMEOUT)) return null;
+                } else {
+                    //2.2: park current thread
+                    parkNodeThread(node, support, throwsIE);
+                }
+            } while (true);
+        } finally {
+            super.removeNode(node);
+        }
+    }
 }
