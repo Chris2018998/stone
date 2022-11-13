b304c60d1904011546a3669872e2d8b546d4f00b
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-13 17:54:07 +0800

    Add files via upload

--- src/main/java/org/stone/shine/synchronizer/locks/AbstractLock.java
+++ src/main/java/org/stone/shine/synchronizer/locks/AbstractLock.java
@@ -30,25 +30,245 @@ import java.util.concurrent.locks.Lock;
  */
 
 abstract class AbstractLock implements Lock {
-    //Lock Acquire Action(ReentrantAction,WriteLockAction,ReadLockAction)
-    private final ResourceAction lockAction;
+    //resource wait Pool
+    private final Object acquireType;
     //resource wait Pool
     private final ResourceWaitPool waitPool;
+    //Lock Acquire Action(ReentrantAction,WriteLockAction,ReadLockAction)
+    private final ResourceAction lockAction;
 
-    //constructor
-    public AbstractLock(boolean fair, ResourceAction lockAction) {
+    //****************************************************************************************************************//
+    //                                          1: constructors (2)                                                   //
+    //****************************************************************************************************************//
+    //constructor1(extend by ReentrantLock)
+    AbstractLock(boolean fair, ResourceAction lockAction) {
         this.lockAction = lockAction;
         this.waitPool = new ResourceWaitPool(fair);
+        this.acquireType = AcquireTypes.TYPE_Exclusive;
     }
 
-    //constructor
-    public AbstractLock(ResourceAction lockAction, ResourceWaitPool waitPool) {
-        this.lockAction = lockAction;
+    //constructor2(extend by WriteLockImpl,ReadLockImpl)
+    AbstractLock(ResourceWaitPool waitPool, ResourceAction lockAction, Object acquireType) {
         this.waitPool = waitPool;
+        this.lockAction = lockAction;
+        this.acquireType = acquireType;
     }
 
     //****************************************************************************************************************//
-    //                                          1: monitor Methods(5)                                                 //
+    //                                          2: lock methods (6)                                                   //
+    //****************************************************************************************************************//
+
+    /**
+     * Acquires the lock.
+     *
+     * <p>If the lock is not available then the current thread becomes
+     * disabled for thread scheduling purposes and lies dormant until the
+     * lock has been acquired.
+     *
+     * <p><b>Implementation Considerations</b>
+     *
+     * <p>A {@code Lock} implementation may be able to detect erroneous use
+     * of the lock, such as an invocation that would cause deadlock, and
+     * may throw an (unchecked) exception in such circumstances.  The
+     * circumstances and the exception type must be documented by that
+     * {@code Lock} implementation.
+     */
+    public void lock() {
+        try {
+            ThreadParkSupport parker = ThreadParkSupport.create();
+            waitPool.acquire(lockAction, 1, parker, false, acquireType, true);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+
+    /**
+     * Acquires the lock unless the current thread is
+     * {@linkplain Thread#interrupt interrupted}.
+     *
+     * <p>Acquires the lock if it is available and returns immediately.
+     *
+     * <p>If the lock is not available then the current thread becomes
+     * disabled for thread scheduling purposes and lies dormant until
+     * one of two things happens:
+     *
+     * <ul>
+     * <li>The lock is acquired by the current thread; or
+     * <li>Some other thread {@linkplain Thread#interrupt interrupts} the
+     * current thread, and interruption of lock acquisition is supported.
+     * </ul>
+     *
+     * <p>If the current thread:
+     * <ul>
+     * <li>has its interrupted status set on entry to this method; or
+     * <li>is {@linkplain Thread#interrupt interrupted} while acquiring the
+     * lock, and interruption of lock acquisition is supported,
+     * </ul>
+     * then {@link InterruptedException} is thrown and the current thread's
+     * interrupted status is cleared.
+     *
+     * <p><b>Implementation Considerations</b>
+     *
+     * <p>The ability to interrupt a lock acquisition in some
+     * implementations may not be possible, and if possible may be an
+     * expensive operation.  The programmer should be aware that this
+     * may be the case. An implementation should document when this is
+     * the case.
+     *
+     * <p>An implementation can favor responding to an interrupt over
+     * normal method return.
+     *
+     * <p>A {@code Lock} implementation may be able to detect
+     * erroneous use of the lock, such as an invocation that would
+     * cause deadlock, and may throw an (unchecked) exception in such
+     * circumstances.  The circumstances and the exception type must
+     * be documented by that {@code Lock} implementation.
+     *
+     * @throws InterruptedException if the current thread is
+     *                              interrupted while acquiring the lock (and interruption
+     *                              of lock acquisition is supported)
+     */
+    public void lockInterruptibly() throws InterruptedException {
+        ThreadParkSupport parker = ThreadParkSupport.create();
+        waitPool.acquire(lockAction, 1, parker, true, acquireType, true);
+    }
+
+    /**
+     * Acquires the lock only if it is free at the time of invocation.
+     *
+     * <p>Acquires the lock if it is available and returns immediately
+     * with the value {@code true}.
+     * If the lock is not available then this method will return
+     * immediately with the value {@code false}.
+     *
+     * <p>A typical usage idiom for this method would be:
+     * <pre> {@code
+     * Lock lock = ...;
+     * if (lock.tryLock()) {
+     *   try {
+     *     // manipulate protected state
+     *   } finally {
+     *     lock.unlock();
+     *   }
+     * } else {
+     *   // perform alternative actions
+     * }}</pre>
+     * <p>
+     * This usage ensures that the lock is unlocked if it was acquired, and
+     * doesn't try to unlock if the lock was not acquired.
+     *
+     * @return {@code true} if the lock was acquired and
+     * {@code false} otherwise
+     */
+    public boolean tryLock() {
+        return waitPool.tryAcquire(lockAction, 1);
+    }
+
+    /**
+     * Acquires the lock if it is free within the given waiting time and the
+     * current thread has not been {@linkplain Thread#interrupt interrupted}.
+     *
+     * <p>If the lock is available this method returns immediately
+     * with the value {@code true}.
+     * If the lock is not available then
+     * the current thread becomes disabled for thread scheduling
+     * purposes and lies dormant until one of three things happens:
+     * <ul>
+     * <li>The lock is acquired by the current thread; or
+     * <li>Some other thread {@linkplain Thread#interrupt interrupts} the
+     * current thread, and interruption of lock acquisition is supported; or
+     * <li>The specified waiting time elapses
+     * </ul>
+     *
+     * <p>If the lock is acquired then the value {@code true} is returned.
+     *
+     * <p>If the current thread:
+     * <ul>
+     * <li>has its interrupted status set on entry to this method; or
+     * <li>is {@linkplain Thread#interrupt interrupted} while acquiring
+     * the lock, and interruption of lock acquisition is supported,
+     * </ul>
+     * then {@link InterruptedException} is thrown and the current thread's
+     * interrupted status is cleared.
+     *
+     * <p>If the specified waiting time elapses then the value {@code false}
+     * is returned.
+     * If the time is
+     * less than or equal to zero, the method will not wait at all.
+     *
+     * <p><b>Implementation Considerations</b>
+     *
+     * <p>The ability to interrupt a lock acquisition in some implementations
+     * may not be possible, and if possible may
+     * be an expensive operation.
+     * The programmer should be aware that this may be the case. An
+     * implementation should document when this is the case.
+     *
+     * <p>An implementation can favor responding to an interrupt over normal
+     * method return, or reporting a timeout.
+     *
+     * <p>A {@code Lock} implementation may be able to detect
+     * erroneous use of the lock, such as an invocation that would cause
+     * deadlock, and may throw an (unchecked) exception in such circumstances.
+     * The circumstances and the exception type must be documented by that
+     * {@code Lock} implementation.
+     *
+     * @param time the maximum time to wait for the lock
+     * @param unit the time unit of the {@code time} argument
+     * @return {@code true} if the lock was acquired and {@code false}
+     * if the waiting time elapsed before the lock was acquired
+     * @throws InterruptedException if the current thread is interrupted
+     *                              while acquiring the lock (and interruption of lock
+     *                              acquisition is supported)
+     */
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        if (unit == null) throw new IllegalArgumentException("time unit can't be null");
+        ThreadParkSupport parker = ThreadParkSupport.create(unit.toNanos(time), false);
+        return waitPool.acquire(lockAction, 1, parker, true, acquireType, true);
+    }
+
+    /**
+     * Releases the lock.
+     *
+     * <p><b>Implementation Considerations</b>
+     *
+     * <p>A {@code Lock} implementation will usually impose
+     * restrictions on which thread can release a lock (typically only the
+     * holder of the lock can release it) and may throw
+     * an (unchecked) exception if the restriction is violated.
+     * Any restrictions and the exception
+     * type must be documented by that {@code Lock} implementation.
+     */
+    public void unlock() {
+        waitPool.release(lockAction, 1);
+    }
+
+    /**
+     * Returns a new {@link Condition} instance that is bound to this
+     * {@code Lock} instance.
+     *
+     * <p>Before waiting on the condition the lock must be held by the
+     * current thread.
+     * A call to {@link Condition#await()} will atomically release the lock
+     * before waiting and re-acquire the lock before the wait returns.
+     *
+     * <p><b>Implementation Considerations</b>
+     *
+     * <p>The exact operation of the {@link Condition} instance depends on
+     * the {@code Lock} implementation and must be documented by that
+     * implementation.
+     *
+     * @return A new {@link Condition} instance for this {@code Lock} instance
+     * @throws UnsupportedOperationException if this {@code Lock}
+     *                                       implementation does not support conditions
+     */
+    public Condition newCondition() {
+        return new LockConditionImpl(this);
+    }
+
+
+    //****************************************************************************************************************//
+    //                                          3: monitor Methods(5)                                                 //
     //****************************************************************************************************************//
     public final boolean isFair() {
         return waitPool.isFair();
@@ -73,42 +293,7 @@ abstract class AbstractLock implements Lock {
     public abstract Thread getHoldThread();
 
     //****************************************************************************************************************//
-    //                                          2: lock methods                                                       //
-    //****************************************************************************************************************//
-    public void lock() {
-        try {
-            ThreadParkSupport parker = ThreadParkSupport.create();
-            waitPool.acquire(lockAction, 1, parker, false, null, true);
-        } catch (Exception e) {
-            //do nothing
-        }
-    }
-
-    public void lockInterruptibly() throws InterruptedException {
-        ThreadParkSupport parker = ThreadParkSupport.create();
-        waitPool.acquire(lockAction, 1, parker, true, null, true);
-    }
-
-    public boolean tryLock() {
-        return waitPool.tryAcquire(lockAction, 1);
-    }
-
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        if (unit == null) throw new IllegalArgumentException("time unit can't be null");
-        ThreadParkSupport parker = ThreadParkSupport.create(unit.toNanos(time), false);
-        return waitPool.acquire(lockAction, 1, parker, true, null, true);
-    }
-
-    public void unlock() {
-        waitPool.release(lockAction, 1);
-    }
-
-    public Condition newCondition() {
-        return new LockConditionImpl(this);
-    }
-
-    //****************************************************************************************************************//
-    //                                        3: Lock Condition Impl                                                  //                                                                                  //
+    //                                        4: Lock Condition Impl                                                  //                                                                                  //
     //****************************************************************************************************************//
     private static class LockConditionImpl extends SignalWaitPool implements Condition {
         private AbstractLock lock;
