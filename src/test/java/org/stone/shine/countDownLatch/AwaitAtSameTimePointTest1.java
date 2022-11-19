0a18b435019c995605cc0a9890c6b13aa70b3739
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-19 19:29:21 +0800

    Add files via upload

---
+++ src/test/java/org/stone/shine/countDownLatch/AwaitAtSameTimePointTest1.java
@@ -0,0 +1,57 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.countDownLatch;
+
+import org.stone.shine.concurrent.CountDownLatch;
+import org.stone.shine.countDownLatch.threads.CountDownThread;
+import org.stone.shine.countDownLatch.threads.SameTimePointToAwaitThread;
+import org.stone.test.TestCase;
+import org.stone.test.TestUtil;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * await without time test case:(all thread wait at same time point:concurrent mock)
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public class AwaitAtSameTimePointTest1 extends TestCase {
+
+    public void test() throws Exception {
+        int count = 100;
+        CountDownLatch latch = new CountDownLatch(count);
+
+        //1:create count down threads
+        CountDownThread[] countDownThreads = new CountDownThread[count];
+        for (int i = 0; i < count; i++) countDownThreads[i] = new CountDownThread(latch);
+
+        //2:cal the expect time point value to wait(5 seconds)
+        long timePointToAWait = System.nanoTime() + TimeUnit.SECONDS.toNanos(5);
+
+        //3:create countWait threads and block them
+        SameTimePointToAwaitThread[] sameTimePointWaitThreads = new SameTimePointToAwaitThread[count];
+        for (int i = 0; i < count; i++) {
+            sameTimePointWaitThreads[i] = new SameTimePointToAwaitThread(latch, timePointToAWait);
+            sameTimePointWaitThreads[i].start();
+        }
+
+        //4: crate count down threads
+        LockSupport.parkNanos(timePointToAWait - System.nanoTime());///wait for the same time(delay 5 seconds)
+        for (int i = 0; i < count; i++) countDownThreads[i].start();
+
+        //5:block main thread
+        latch.await();
+
+        //6:count value should be zero
+        TestUtil.assertError("final count value expect value:%s,actual value:%s", 0, (int) latch.getCount());
+    }
+}
---
+++ src/test/java/org/stone/shine/countDownLatch/threads/GeneralAwaitThread.java
@@ -0,0 +1,62 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.countDownLatch.threads;
+
+import org.stone.shine.concurrent.CountDownLatch;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * CountDownLatch Mock Thread
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+public class GeneralAwaitThread extends BaseThread {
+    private long timeout;
+    private TimeUnit timeUnit;
+    private boolean hasTimeout;
+    private InterruptedException cause;
+
+    public GeneralAwaitThread(CountDownLatch latch) {
+        super(latch);
+    }
+
+    public GeneralAwaitThread(CountDownLatch latch, long timeout, TimeUnit timeUnit) {
+        super(latch);
+        this.timeout = timeout;
+        this.timeUnit = timeUnit;
+    }
+
+    public boolean isHasTimeout() {
+        return hasTimeout;
+    }
+
+    public InterruptedException getCause() {
+        return cause;
+    }
+
+    public void run() {
+        if (timeout == 0) {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                cause = e;
+            }
+        } else {
+            try {
+                this.hasTimeout = !latch.await(timeout, timeUnit);
+            } catch (InterruptedException e) {
+                cause = e;
+            }
+        }
+    }
+}
