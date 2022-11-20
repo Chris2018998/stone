5ad83ebe98d85694de6ab811096682cddfc97dfb
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-20 23:12:21 +0800

    Add files via upload

---
+++ src/main/java/org/stone/shine/concurrent/LinkedTransferQueue.java
@@ -0,0 +1,22 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.concurrent;
+
+/**
+ * LinkedTransferQueue implementation by wait Pool
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+//public class LinkedTransferQueue<E> extends AbstractQueue<E> implements TransferQueue<E>, java.io.Serializable {
+//
+//
+//}
