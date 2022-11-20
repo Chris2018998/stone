8d75d88e122ed0e3985a2a753c3728d679ab7e4d
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-20 21:05:37 +0800

    Add files via upload

---
+++ src/test/java/org/stone/shine/concurrent/locks/reentrantReadWriteLock/threads/LockAcquireThread.java
@@ -0,0 +1,35 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.concurrent.locks.reentrantReadWriteLock.threads;
+
+import org.stone.shine.concurrent.ConcurrentMockThread;
+import org.stone.shine.synchronizer.locks.ReentrantReadWriteLock;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * mock thread
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public class LockAcquireThread extends ConcurrentMockThread {
+    private ReentrantReadWriteLock lock;
+
+    public LockAcquireThread(ReentrantReadWriteLock lock, String methodName) {
+        super(methodName);
+        this.lock = lock;
+    }
+
+    public LockAcquireThread(ReentrantReadWriteLock lock, String methodName, long timeout, TimeUnit timeUnit) {
+        super(methodName, timeout, timeUnit);
+        this.lock = lock;
+    }
+}
