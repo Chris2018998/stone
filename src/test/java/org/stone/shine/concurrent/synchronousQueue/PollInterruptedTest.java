68bd6c00478ac6f3a70a50fb5bd38960746544f5
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-20 15:32:45 +0800

    Add files via upload

---
+++ src/test/java/org/stone/shine/concurrent/synchronousQueue/PollInterruptedTest.java
@@ -0,0 +1,47 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.concurrent.synchronousQueue;
+
+import org.stone.shine.concurrent.SynchronousQueue;
+import org.stone.shine.concurrent.synchronousQueue.threads.PollThread;
+import org.stone.test.TestCase;
+import org.stone.test.TestUtil;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * synchronousQueue Test case
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+public class PollInterruptedTest extends TestCase {
+
+    public void test() throws Exception {
+        SynchronousQueue queue = new SynchronousQueue(true);
+
+        //2:create one mock Thread
+        PollThread mockThread = new PollThread(queue, "take");
+        mockThread.start();
+
+        //3:park main thread 3 seconds and check mock thread result
+        LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(1));
+        if (mockThread.getState() != Thread.State.WAITING)
+            TestUtil.assertError("Test failed,put thread not in waiting");
+
+        //4:interrupt the mock thread
+        mockThread.interrupt();
+        LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(1));
+        if (mockThread.getInterruptedException() == null)
+            TestUtil.assertError("Test failed,put thread not be interrupted");
+    }
+}
