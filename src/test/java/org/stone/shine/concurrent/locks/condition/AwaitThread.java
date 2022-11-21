d24aa20c38b27b73ec86da1a5afd12c68b211685
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-21 22:52:55 +0800

    Add files via upload

--- src/test/java/org/stone/shine/concurrent/locks/condition/AwaitThread.java
+++ src/test/java/org/stone/shine/concurrent/locks/condition/AwaitThread.java
@@ -12,6 +12,7 @@ package org.stone.shine.concurrent.locks.condition;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
 /**
  * condition test thread
@@ -20,23 +21,38 @@ import java.util.concurrent.locks.Condition;
  * @version 1.0
  */
 public class AwaitThread extends BaseThread {
+    private Lock lock;
+    private boolean enterLock;
+    private long awaitOverTime;
     private Date deadline;
 
-    public AwaitThread(Condition condition, String methodName) {
+    public AwaitThread(Lock lock, Condition condition, String methodName) {
         super(condition, methodName);
+        this.lock = lock;
     }
 
-    public AwaitThread(Condition condition, String methodName, Date deadline) {
+    public AwaitThread(Lock lock, Condition condition, String methodName, Date deadline) {
         super(condition, methodName);
         this.deadline = deadline;
+        this.lock = lock;
     }
 
-    public AwaitThread(Condition condition, String methodName, long timeout, TimeUnit timeUnit) {
+    public AwaitThread(Lock lock, Condition condition, String methodName, long timeout, TimeUnit timeUnit) {
         super(condition, methodName, timeout, timeUnit);
     }
 
+    public boolean isEnterLock() {
+        return enterLock;
+    }
+
+    public long getAwaitOverTime() {
+        return awaitOverTime;
+    }
+
     public void run() {
+        lock.lock();
         try {
+            this.enterLock = true;
             if ("await".equals(methodName) && timeUnit != null) {
                 this.result = condition.await(timeout, timeUnit);
             } else if ("await".equals(methodName)) {
@@ -48,8 +64,11 @@ public class AwaitThread extends BaseThread {
             } else if ("awaitUntil".equals(methodName)) {
                 this.result = condition.awaitUntil(deadline);
             }
+            this.awaitOverTime = System.currentTimeMillis();
         } catch (InterruptedException e) {
             this.interruptedException = e;
+        } finally {
+            lock.unlock();
         }
     }
 }
