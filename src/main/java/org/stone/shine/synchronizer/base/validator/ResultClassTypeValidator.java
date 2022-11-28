a71647ba272933768e1ad2487c520ddbbc1f5ec0
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-29 07:59:51 +0800

    Add files via upload

---
+++ src/main/java/org/stone/shine/synchronizer/base/validator/ResultClassTypeValidator.java
@@ -0,0 +1,31 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.synchronizer.base.validator;
+
+/**
+ * result class type validator
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+public class ResultClassTypeValidator extends BaseValidator {
+    private Class classType;
+
+    public ResultClassTypeValidator(Class classType, Object resultOnTimeout) {
+        super(resultOnTimeout);
+        this.classType = classType;
+        if (classType == null) throw new IllegalArgumentException("result class type can't be null");
+    }
+
+    public final boolean match(Object result) {
+        return classType.isInstance(result);
+    }
+}
