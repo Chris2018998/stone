fbd664ce6746f2af6117b1b7a092d165f5c4cd59
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-29 20:20:35 +0800

    Add files via upload

--- src/main/java/org/stone/util/UnsafeUtil.java
+++ src/main/java/org/stone/util/UnsafeUtil.java
@@ -20,19 +20,79 @@ import java.lang.reflect.Field;
  * @version 1.0
  */
 public class UnsafeUtil {
-    private static Unsafe unsafe;
+    private static Unsafe u;
 
     static {
         try {
             Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
             theUnsafe.setAccessible(true);
-            unsafe = (Unsafe) theUnsafe.get(null);
+            u = (Unsafe) theUnsafe.get(null);
         } catch (Exception e) {
             throw new Error("Failed to get Unsafe", e);
         }
     }
 
     public static Unsafe getUnsafe() {
-        return UnsafeUtil.unsafe;
+        return UnsafeUtil.u;
+    }
+
+    //****************************************************************************************************************//
+    //                                            field offset                                                        //
+    //****************************************************************************************************************//
+    public static long objectFieldOffset(Class beanClass, String fieldName) throws Exception {
+        return u.objectFieldOffset(beanClass.getDeclaredField(fieldName));
+    }
+
+    public static long staticFieldOffset(Class beanClass, String fieldName) throws Exception {
+        return u.staticFieldOffset(beanClass.getDeclaredField(fieldName));
+    }
+
+    public static Object staticFieldBase(Class beanClass, String fieldName) throws Exception {
+        return u.staticFieldBase(beanClass.getDeclaredField(fieldName));
+    }
+
+    //****************************************************************************************************************//
+    //                                            volatile(int)                                                       //
+    //****************************************************************************************************************//
+    public static int getIntVolatile(Object object, long offset) {
+        return u.getIntVolatile(object, offset);
+    }
+
+    public static void putIntVolatile(Object object, long offset, int update) {
+        u.putIntVolatile(object, offset, update);
+    }
+
+    public static boolean compareAndSwapInt(Object object, long offset, int expect, int update) {
+        return u.compareAndSwapInt(object, offset, expect, update);
+    }
+
+    //****************************************************************************************************************//
+    //                                            volatile(long)                                                       //
+    //****************************************************************************************************************//
+    public static long getLongVolatile(Object object, long offset) {
+        return u.getLongVolatile(object, offset);
+    }
+
+    public static void putLongVolatile(Object object, long offset, long update) {
+        u.putLongVolatile(object, offset, update);
+    }
+
+    public static boolean compareAndSwapLong(Object object, long offset, long expect, long update) {
+        return u.compareAndSwapLong(object, offset, expect, update);
+    }
+
+    //****************************************************************************************************************//
+    //                                            volatile(Object)                                                    //
+    //****************************************************************************************************************//
+    public static Object getObjectVolatile(Object object, long offset) {
+        return u.getObjectVolatile(object, offset);
+    }
+
+    public static void putObjectVolatile(Object object, long offset, Object update) {
+        u.putObjectVolatile(object, offset, update);
+    }
+
+    public static boolean compareAndSwapObject(Object object, long offset, Object expect, Object update) {
+        return u.compareAndSwapObject(object, offset, expect, update);
     }
 }
