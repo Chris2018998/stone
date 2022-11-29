36627bd3fa9e6f4e21ced41519a3d9a6bafbecac
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-11-29 19:38:08 +0800

    Add files via upload

---
+++ src/main/java/org/stone/shine/synchronizer/ThreadParkSupport.java
@@ -0,0 +1,232 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.synchronizer;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Time parker class,supply three Implementation with park methods of {@link LockSupport} class
+ * 1:LockSupport.park
+ * 2:LockSupport.parkNanos
+ * 3:LockSupport.parkUntil
+ *
+ * <p>
+ * Class objective:reduce spin similar code (for example:some await methods in class:{@link java.util.concurrent.locks.AbstractQueuedSynchronizer})
+ * Usage demo:
+ * <pre>{@code
+ * class ConditionX {
+ *    public final void await() throws InterruptedException {
+ *       ThreadParkSupport parker = ThreadParkSupport.create();
+ *       await(parker):
+ *    }
+ *    public final void await(long nanosTimeout) throws InterruptedException {
+ *      ThreadParkSupport parker = ThreadParkSupport.create(nanosTimeout,false);
+ *      await(parker):
+ *    }
+ *    public final boolean awaitUntil(Date deadline) throws InterruptedException {
+ *      ThreadParkSupport parker = ThreadParkSupport.create(deadline.getParkTime(),true);
+ *      await(parker):
+ *    }
+ *   public final boolean await(long parkTime, TimeUnit unit)throws InterruptedException {
+ *      long nanosTimeout = unit.toNanos(timeout);
+ *      ThreadParkSupport parker = ThreadParkSupport.create(nanosTimeout,false);
+ *      await(parker):
+ *   }
+ *   //spin control
+ *   private void await(ThreadParkSupport  parker)throws InterruptedException {
+ *      //spin source code.........
+ *   }
+ *  }//class end
+ * }//code tag
+ * </pre>
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+//********************************************************************************************************************//
+//                          1:Implementation based on park method{@code LockSupport.park}                             //
+//********************************************************************************************************************//
+public class ThreadParkSupport implements Cloneable {
+    private static final long spinForTimeoutThreshold = 1000L;
+    protected long deadline;
+    protected long parkTime = 1L;//a initialized value,calculated before park,if less than zero or equals zero means timeout
+    protected Object blocker;
+    protected boolean interrupted;
+
+    //****************************************************************************************************************//
+    //                                              constructors(2)                                                   //
+    //****************************************************************************************************************//
+    ThreadParkSupport() {
+    }
+
+    ThreadParkSupport(Object blocker) {
+        this.blocker = blocker;
+    }
+
+    //****************************************************************************************************************//
+    //                                           Factory  methods(4)                                                  //
+    //****************************************************************************************************************//
+    public static ThreadParkSupport create(long time, boolean isMilliseconds) {
+        if (time <= 0)
+            return new ThreadParkSupport();
+        else if (isMilliseconds)
+            return new MillisecondsUtilParkSupport(time);
+        else
+            return new NanoSecondsParkSupport(time);
+    }
+
+    public static ThreadParkSupport create(long time, boolean isMilliseconds, Object blocker) {
+        if (time <= 0)
+            return new ThreadObjectParkSupport(blocker);
+        else if (isMilliseconds)
+            return new MillisecondsObjectUtilParkSupport(time, blocker);
+        else
+            return new NanoSecondsObjectParkSupport(time, blocker);
+    }
+
+    //****************************************************************************************************************//
+    //                                           Factory  methods(end)                                                //
+    //****************************************************************************************************************//
+    public boolean park() {
+        LockSupport.park();
+        return interrupted = Thread.interrupted();
+    }
+
+    public final long getDeadline() {
+        return deadline;
+    }
+
+    public final long getParkTime() {
+        return parkTime;
+    }
+
+    public final boolean isTimeout() {
+        return parkTime <= 0;
+    }
+
+    //calculate park time for{@code LockSupport.park},true time value is valid
+    public boolean calculateParkTime() {
+        return true;
+    }
+
+    public final boolean isInterrupted() {
+        return interrupted;
+    }
+
+    public String toString() {
+        return "Implementation with LockSupport.park()";
+    }
+
+    //Thead Park with block object for{@code LockSupport.park(blocker)}
+    private static class ThreadObjectParkSupport extends ThreadParkSupport {
+        ThreadObjectParkSupport(Object blocker) {
+            super(blocker);
+        }
+
+        public boolean park() {
+            LockSupport.park(blocker);
+            return interrupted = Thread.interrupted();
+        }
+
+        public String toString() {
+            return "Implementation with LockSupport.park(blocker)";
+        }
+    }
+
+    //****************************************************************************************************************//
+    //                     2: Implementation based on park method{@code LockSupport.parkNanos}                        //
+    //****************************************************************************************************************//
+    private static class NanoSecondsParkSupport extends ThreadParkSupport {
+        NanoSecondsParkSupport(long nanoTime) {
+            this.deadline = System.nanoTime() + nanoTime;
+        }
+
+        //true means that time point before deadline
+        public final boolean calculateParkTime() {
+            this.parkTime = deadline - System.nanoTime();
+            return parkTime > 0;
+        }
+
+        public boolean park() {
+            if (parkTime > spinForTimeoutThreshold) {
+                LockSupport.parkNanos(parkTime);
+                this.interrupted = Thread.interrupted();
+            }
+            return interrupted;
+        }
+
+        public String toString() {
+            return "Implementation with LockSupport.parkNanos(time)";
+        }
+    }
+
+    private static class NanoSecondsObjectParkSupport extends NanoSecondsParkSupport {
+        NanoSecondsObjectParkSupport(long nanoTime, Object blocker) {
+            super(nanoTime);
+            this.blocker = blocker;
+        }
+
+        public final boolean park() {
+            if (parkTime > spinForTimeoutThreshold) {
+                LockSupport.parkNanos(parkTime);
+                return interrupted = Thread.interrupted();
+            } else {
+                return interrupted;
+            }
+        }
+
+        public String toString() {
+            return "Implementation with LockSupport.parkNanos(time,blocker)";
+        }
+    }
+
+    //****************************************************************************************************************//
+    //                        3: Implementation based on park method{@code LockSupport.parkUntil}                     //
+    //****************************************************************************************************************//
+    private static class MillisecondsUtilParkSupport extends ThreadParkSupport {
+        MillisecondsUtilParkSupport(long deadline) {
+            this.deadline = deadline;
+        }
+
+        public final boolean calculateParkTime() {
+            this.parkTime = deadline - System.currentTimeMillis();
+            return parkTime > 0;
+        }
+
+        public boolean park() {
+            LockSupport.parkUntil(deadline);
+            return interrupted = Thread.interrupted();
+        }
+
+        public String toString() {
+            return "Implementation with LockSupport.parkUntil(deadline)";
+        }
+    }
+
+    private static class MillisecondsObjectUtilParkSupport extends MillisecondsUtilParkSupport {
+        MillisecondsObjectUtilParkSupport(long deadline, Object blocker) {
+            super(deadline);
+            this.blocker = blocker;
+        }
+
+        public final boolean park() {
+            LockSupport.parkUntil(blocker, deadline);
+            return interrupted = Thread.interrupted();
+        }
+
+        public String toString() {
+            return "Implementation with LockSupport.parkUntil(deadline,blocker)";
+        }
+    }
+}
+
+
+
---
+++ src/main/java/org/stone/shine/synchronizer/base/ResultWaitPool.java
@@ -0,0 +1,127 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.synchronizer.base;
+
+import org.stone.shine.synchronizer.*;
+import org.stone.shine.synchronizer.base.validator.ResultEqualsValidator;
+
+/**
+ * Result call executed inside pool and compare result of call with expected parameter,if equals,return true
+ * and leave from pool,not equals,then join in wait queue util wakeup from other and execute call again and compare.
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public class ResultWaitPool extends ThreadWaitPool {
+
+    //true,use fair mode to execute call
+    private final boolean fair;
+
+    //call result validator(equals validator is default)
+    private final ResultValidator validator;
+
+    //****************************************************************************************************************//
+    //                                          1: constructors(3)                                                    //
+    //****************************************************************************************************************//
+    public ResultWaitPool() {
+        this(false);
+    }
+
+    public ResultWaitPool(boolean fair) {
+        this(fair, new ResultEqualsValidator(true, false));
+    }
+
+    public ResultWaitPool(boolean fair, ResultValidator validator) {
+        this.fair = fair;
+        this.validator = validator;
+    }
+
+    public final boolean isFair() {
+        return this.fair;
+    }
+
+    /**
+     * execute the call inside pool and compare its result with expected parameter value,if equivalence that this
+     * method return true,not equals that wait timeout and return false.
+     *
+     * @param call   executed in pool to get result
+     * @param arg    call argument
+     * @param config thread wait config
+     * @return object, if call result check passed by validator
+     * @throws Exception exception from call or InterruptedException after thread park
+     */
+    public final Object doCall(ResultCall call, Object arg, ThreadWaitConfig config) throws Exception {
+        return this.doCall(call, arg, config, validator);
+    }
+
+    /**
+     * execute the call inside pool and compare its result with expected parameter value,if equivalence that this
+     * method return true,not equals that wait timeout and return false.
+     *
+     * @param call      executed in pool to get result
+     * @param arg       call argument
+     * @param config    thread wait config
+     * @param validator result call validator
+     * @return object, if call result check passed by validator
+     * @throws Exception exception from call or InterruptedException after thread park
+     */
+    public final Object doCall(ResultCall call, Object arg, ThreadWaitConfig config, ResultValidator validator) throws Exception {
+        //1:check call parameter
+        if (call == null) throw new IllegalArgumentException("call can't be null");
+        if (config == null) throw new IllegalArgumentException("wait config can't be null");
+        if (validator == null) throw new IllegalArgumentException("result validator can't be null");
+
+        //2:try to execute call
+        if (config.isOutsideOfWaitPool()) {
+            if (!fair || !this.hasQueuedThreads()) {
+                Object result = call.call(arg);
+                if (validator.match(result)) return result;
+            }
+            super.appendNode(config.getThreadNode());
+        }
+
+        //3:get wait node from config object
+        ThreadNode node = config.getThreadNode();
+
+        //4:get control parameters from config
+        boolean throwsIE = config.isThrowsIE();
+        boolean wakeupOtherOnIE = config.isTransferSignalOnIE();
+
+        //5:create thread parker
+        ThreadParkSupport parker = config.getThreadParkSupport();
+
+        //6:spin control
+        try {
+            do {
+                //6.1:if got a signal then execute call
+                Object result = call.call(arg);
+                if (validator.match(result)) return result;
+
+                //6.2:read node state
+                Object state = node.getState();
+
+                //6.3:timeout test
+                if (parker.isTimeout()) {
+                    //6.3.1:try cas state from null to TIMEOUT(more static states,@see{@link ThreadNodeState})then return false(abandon)
+                    if (ThreadNodeUpdater.casNodeState(node, state, ThreadNodeState.TIMEOUT))
+                        return validator.resultOnTimeout();
+                } else if (state != null) {//6.4:reach here means not got expected value from call,then rest to continue waiting
+                    node.setState(null);
+                    Thread.yield();
+                } else {//here: state == null
+                    //6.5:park current thread(if interrupted then transfer the got state value to another waiter)
+                    parkNodeThread(node, parker, throwsIE, wakeupOtherOnIE);
+                }
+            } while (true);
+        } finally {
+            super.removeNode(node);
+        }
+    }
+}
