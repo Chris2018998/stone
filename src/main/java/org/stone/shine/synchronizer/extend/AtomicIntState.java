085cd0ed8bce3da21bbded0e778f54eaa022aa4c
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-12-02 13:25:01 +0800

    Add files via upload

---
+++ src/main/java/org/stone/shine/synchronizer/extend/AtomicIntState.java
@@ -0,0 +1,54 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.synchronizer.extend;
+
+import org.stone.util.UnsafeAdaptor;
+import org.stone.util.UnsafeAdaptorFactory;
+
+/**
+ * resource state,which be similar to atomic field in {@link java.util.concurrent.locks.AbstractQueuedSynchronizer}class
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+public class AtomicIntState {
+    private final static UnsafeAdaptor U = UnsafeAdaptorFactory.get();
+    private final static long offset;
+
+    static {
+        try {
+            offset = U.objectFieldOffset(AtomicIntState.class.getDeclaredField("state"));
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
+
+    private volatile int state;
+
+    public AtomicIntState() {
+    }
+
+    public AtomicIntState(int state) {
+        this.state = state;
+    }
+
+    public final int getState() {
+        return state;
+    }
+
+    public final void setState(int newState) {
+        U.putIntVolatile(this, offset, newState);
+    }
+
+    public final boolean compareAndSetState(int expect, int update) {
+        return U.compareAndSwapInt(this, offset, expect, update);
+    }
+}
