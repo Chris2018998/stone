be9a9e32741d839448d1838013c26142ff507d4c
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-12-09 07:10:58 +0800

    Add files via upload

---
+++ src/main/java/org/stone/shine/synchronizer/base/validator/AnyValidator.java
@@ -0,0 +1,32 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.synchronizer.base.validator;
+
+/**
+ * any Validator
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public final class AnyValidator extends BaseValidator {
+
+    public AnyValidator() {
+        super(null);
+    }
+
+    public AnyValidator(Object resultOnTimeout) {
+        super(resultOnTimeout);
+    }
+
+    //check call result or state is whether expected
+    public boolean isExpect(Object result) {
+        return true;
+    }
+}
