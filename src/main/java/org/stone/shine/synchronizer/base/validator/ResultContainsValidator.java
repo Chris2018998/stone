ad087aa048f0ee70c026681da9f0bc0dfdc6ba67
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-12-09 12:40:54 +0800

    Update ResultContainsValidator.java

--- src/main/java/org/stone/shine/synchronizer/base/validator/ResultContainsValidator.java
+++ src/main/java/org/stone/shine/synchronizer/base/validator/ResultContainsValidator.java
@@ -21,7 +21,7 @@ import java.util.Collection;
 public final class ResultContainsValidator extends CollectionValidator {
 
     public ResultContainsValidator(Collection collection){
-       this(collection);
+       super(collection);
     }
     
     public ResultContainsValidator(Collection collection, Object resultOnTimeout) {
