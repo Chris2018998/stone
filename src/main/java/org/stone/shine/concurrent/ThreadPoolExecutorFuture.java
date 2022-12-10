6c1d4c49f2ee992b76bf68bea2079bcb36caba26
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-12-10 22:10:36 +0800

    Add files via upload

--- src/main/java/org/stone/shine/concurrent/ThreadPoolExecutorFuture.java
+++ src/main/java/org/stone/shine/concurrent/ThreadPoolExecutorFuture.java
@@ -9,10 +9,10 @@
  */
 package org.stone.shine.concurrent;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import org.stone.shine.synchronizer.ThreadWaitConfig;
+
+import java.util.UUID;
+import java.util.concurrent.*;
 
 /**
  * Future Impl by wait pool
@@ -22,6 +22,54 @@ import java.util.concurrent.TimeoutException;
  */
 
 public class ThreadPoolExecutorFuture<V> implements Future<V> {
+    //task in executing
+    private static final int State_Doing = 1;
+    //task done
+    private static final int State_Result = 2;
+    //task canceled
+    private static final int State_Canceled = 3;
+    //task execution exception
+    private static final int State_ExecutionException = 4;
+
+    private final UUID taskId;//task uuid set from executor
+    private final ThreadPoolExecutor executor;
+    private volatile int state;
+
+    private V result;
+    private ExecutionException executionException;
+
+    ThreadPoolExecutorFuture(UUID taskId, ThreadPoolExecutor executor) {
+        this.taskId = taskId;
+        this.executor = executor;
+        this.state = State_Doing;
+    }
+
+    //****************************************************************************************************************//
+    //                                          2: Future methods                                                     //
+    //****************************************************************************************************************//
+
+    /**
+     * Returns {@code true} if this task completed.
+     * <p>
+     * Completion may be due to normal termination, an exception, or
+     * cancellation -- in all of these cases, this method will return
+     * {@code true}.
+     *
+     * @return {@code true} if this task completed
+     */
+    public boolean isDone() {
+        return state >= State_Result;
+    }
+
+    /**
+     * Returns {@code true} if this task was cancelled before it completed
+     * normally.
+     *
+     * @return {@code true} if this task was cancelled before it completed
+     */
+    public boolean isCancelled() {
+        return state == State_Canceled;
+    }
 
     /**
      * Attempts to cancel execution of this task.  This attempt will
@@ -45,32 +93,7 @@ public class ThreadPoolExecutorFuture<V> implements Future<V> {
      * {@code true} otherwise
      */
     public boolean cancel(boolean mayInterruptIfRunning) {
-        //@todo
-        return false;
-    }
-
-    /**
-     * Returns {@code true} if this task was cancelled before it completed
-     * normally.
-     *
-     * @return {@code true} if this task was cancelled before it completed
-     */
-    public boolean isCancelled() {
-        //@todo
-        return false;
-    }
-
-    /**
-     * Returns {@code true} if this task completed.
-     * <p>
-     * Completion may be due to normal termination, an exception, or
-     * cancellation -- in all of these cases, this method will return
-     * {@code true}.
-     *
-     * @return {@code true} if this task completed
-     */
-    public boolean isDone() {
-        //@todo
+        //@todo if Interrupt success,the task executor will abandon task
         return false;
     }
 
@@ -79,15 +102,22 @@ public class ThreadPoolExecutorFuture<V> implements Future<V> {
      * retrieves its result.
      *
      * @return the computed result
+     * @throws InterruptedException,                      which from wait,just thrown once;if the current thread was interrupted while waiting
+     * @throws ExecutionException,                        which from executor,if the computation threw an exception
      * @throws java.util.concurrent.CancellationException if the computation was cancelled
-     * @throws ExecutionException                         if the computation threw an
-     *                                                    exception
-     * @throws InterruptedException                       if the current thread was interrupted
-     *                                                    while waiting
      */
     public V get() throws InterruptedException, ExecutionException {
-        //@todo
-        return null;
+        //step1:read result by state code
+        int code = state;
+        if (code != State_Doing) return getResult(code);
+
+        //step2:wait result
+        ThreadWaitConfig config = new ThreadWaitConfig();
+        config.setNodeType(taskId);
+        //@todo wait int pool(InterruptedException can be thrown from wait pool)
+
+        //step3:read result by state code
+        return getResult(state);
     }
 
     /**
@@ -98,15 +128,49 @@ public class ThreadPoolExecutorFuture<V> implements Future<V> {
      * @param unit    the time unit of the timeout argument
      * @return the computed result
      * @throws java.util.concurrent.CancellationException if the computation was cancelled
-     * @throws ExecutionException                         if the computation threw an
-     *                                                    exception
-     * @throws InterruptedException                       if the current thread was interrupted
-     *                                                    while waiting
+     * @throws InterruptedException                       if the current thread was interrupted while waiting
      * @throws TimeoutException                           if the wait timed out
+     * @throws ExecutionException                         if the computation threw an  exception
      */
-    public V get(long timeout, TimeUnit unit)
-            throws InterruptedException, ExecutionException, TimeoutException {
-        //@todo
-        return null;
+    public V get(long timeout, TimeUnit unit) throws InterruptedException, TimeoutException, ExecutionException {
+        //step1:read result by state code
+        int code = state;
+        if (code != State_Doing) return getResult(code);
+
+        //step2:wait result
+        ThreadWaitConfig config = new ThreadWaitConfig(timeout, unit);
+        config.setNodeType(taskId);
+        //@todo wait int pool(InterruptedException can be thrown from wait pool)
+        if (config.getThreadParkSupport().isTimeout()) throw new TimeoutException();
+
+        //step3:read result by state code
+        return getResult(state);
+    }
+
+    //****************************************************************************************************************//
+    //                                          3: result get/set methods                                             //
+    //****************************************************************************************************************//
+    //set by ThreadPoolExecutor
+    void setResult(V result) {
+        if (state == State_Doing) {
+            this.result = result;
+            this.state = State_Result;
+        }
+    }
+
+    //set by ThreadPoolExecutor
+    void setExecutionException(Throwable e) {
+        if (state == State_Doing) {
+            this.executionException = new ExecutionException(e);
+            this.state = State_ExecutionException;
+        }
+    }
+
+    //call in get methods
+    private V getResult(int code) throws ExecutionException {
+        if (code == State_Result) return result;
+        if (code == State_Canceled) throw new CancellationException();
+        if (code == State_ExecutionException) throw executionException;
+        throw new InternalError();
     }
 }
