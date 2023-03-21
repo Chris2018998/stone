e168d85acf853042da38c9c0954a15700453c07b
Chris <chris2018998@tom.com>
2023-03-21 13:24:57 +0000

    Upload New File

---
+++ src/main/java/org/stone/beetp/pool/PoolWorkerThread.java
@@ -0,0 +1,31 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.beetp.pool;
+
+/**
+ * Task Worker thread
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+final class PoolWorkerThread extends Thread {
+    private final DefaultTaskPool pool;
+    private int state;
+
+    public PoolWorkerThread(DefaultTaskPool pool, String name, boolean daemon) {
+        this.pool = pool;
+        super.setName(name);
+        super.setDaemon(daemon);
+    }
+
+    public void run() {
+
+    }
+}
