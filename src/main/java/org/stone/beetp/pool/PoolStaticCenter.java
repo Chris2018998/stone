e9462283d144c2479fd79fd237e699c1a8caedd7
Chris2018998 <chris2018998@tom.com>
2023-03-27 15:41:53 +0000

    Replace PoolStaticCenter.java

--- src/main/java/org/stone/beetp/pool/PoolStaticCenter.java
+++ src/main/java/org/stone/beetp/pool/PoolStaticCenter.java
@@ -27,16 +27,12 @@ public class PoolStaticCenter {
     static final int TASK_EXCEPTIONAL = 3;
     static final int TASK_COMPLETED = 4;
     //pool state
-    static final int POOL_NEW = 0;
-    static final int POOL_STARTING = 1;
-    static final int POOL_READY = 2;
+    static final int POOL_READY = 1;
+    static final int POOL_CLEARING = 2;
     static final int POOL_TERMINATING = 3;
     static final int POOL_TERMINATED = 4;
-    static final int POOL_CLEARING = 5;
     //worker state
     static final int WORKER_IDLE = 0;
     static final int WORKER_RUNNING = 1;
     static final int WORKER_TERMINATED = 2;
-
-
 }
