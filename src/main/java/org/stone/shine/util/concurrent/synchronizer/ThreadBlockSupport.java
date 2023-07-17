1eb3f09ee54edab34de0371abb9067c59f682464
Chris2018998 <chris2018998@tom.com>
2023-07-17 14:19:46 +0000

    Upload New File

---
+++ src/main/java/org/stone/shine/util/concurrent/synchronizer/ThreadBlockSupport.java
@@ -0,0 +1,217 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.shine.util.concurrent.synchronizer;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.locks.LockSupport.*;
+
+/**
+ * Time parker class,supply three Implementation with park methods of {@link @java.util.concurrent.locks.LockSupport} class
+ * 1:park
+ * 2:blockNanos
+ * 3:parkUntil
+ *
+ * <p>
+ * Class objective:reduce spin similar code (for example:some await methods in class:{@link @java.util.concurrent.locks.AbstractQueuedSynchronizer})
+ * Usage demo:
+ * <pre>{@code
+ * class ConditionX {
+ *    public final void await() throws InterruptedException {
+ *       ThreadBlockSupport parker = ThreadBlockSupport.create();
+ *       await(parker):
+ *    }
+ *    public final void await(long nanosTimeout) throws InterruptedException {
+ *      ThreadBlockSupport parker = ThreadBlockSupport.create(nanosTimeout,false);
+ *      await(parker):
+ *    }
+ *    public final boolean awaitUntil(Date deadlineNanos) throws InterruptedException {
+ *      ThreadBlockSupport parker = ThreadBlockSupport.create(deadlineNanos.getBlockNanos(),true);
+ *      await(parker):
+ *    }
+ *   public final boolean await(long blockNanos, TimeUnit unit)throws InterruptedException {
+ *      long nanosTimeout = unit.toNanos(timeout);
+ *      ThreadBlockSupport parker = ThreadBlockSupport.create(nanosTimeout,false);
+ *      await(parker):
+ *   }
+ *   //spin control
+ *   private void await(ThreadBlockSupport  parker)throws InterruptedException {
+ *      //spin source code.........
+ *   }
+ *  }//class end
+ * }//code tag
+ * </pre>
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+
+public class ThreadBlockSupport {
+    private static final long minSpinForTimeoutThreshold = 1024L;
+    long blockNanos = minSpinForTimeoutThreshold;//compute before parking,if less than zero or equals zero means timeout
+    long deadlineNanos;//nanoseconds
+    Object blockObject;
+    boolean interrupted;
+
+    ThreadBlockSupport() {
+    }
+
+    public final long getBlockNanos() {
+        return blockNanos;
+    }
+
+    public final long getDeadlineNanos() {
+        return deadlineNanos;
+    }
+
+    public final boolean isTimeout() {
+        return blockNanos <= 0;
+    }
+
+    public final boolean isInterrupted() {
+        return interrupted;
+    }
+
+    public String toString() {
+        return "Implementation with method 'park()'";
+    }
+
+    public void reset() {
+        this.blockNanos = minSpinForTimeoutThreshold;
+        this.interrupted = false;
+    }
+
+    public long computeBlockTime() {
+        return minSpinForTimeoutThreshold;
+    }
+
+    public boolean block() {
+        park();
+        return this.interrupted = Thread.interrupted();
+    }
+
+    //****************************************************************************************************************//
+    //                                           object block Implement                                               //
+    //****************************************************************************************************************//
+    static class ThreadBlockSupport2 extends ThreadBlockSupport {
+        ThreadBlockSupport2(Object blocker) {
+            this.blockObject = blocker;
+        }
+
+        public final boolean block() {
+            park(blockObject);
+            return this.interrupted = Thread.interrupted();
+        }
+
+        public String toString() {
+            return "Implementation with method 'park(blockObject)'";
+        }
+    }
+
+    //****************************************************************************************************************//
+    //                                            NanoSeconds block Implement                                         //
+    //****************************************************************************************************************//
+    static class NanoSecondsBlockSupport extends ThreadBlockSupport {
+        private final long nanoTime;
+
+        NanoSecondsBlockSupport(long nanoTime) {
+            this.nanoTime = nanoTime;
+            this.deadlineNanos = System.nanoTime() + nanoTime;
+        }
+
+        public final void reset() {
+            super.reset();
+            this.deadlineNanos = System.nanoTime() + nanoTime;
+        }
+
+        public long computeBlockTime() {
+            return this.blockNanos = deadlineNanos - System.nanoTime();
+        }
+
+        public boolean block() {
+            parkNanos(blockNanos);
+            return this.interrupted = Thread.interrupted();
+        }
+
+        public String toString() {
+            return "Implementation with method 'parkNanos(time)'";
+        }
+    }
+
+    //****************************************************************************************************************//
+    //                                    NanoSeconds blockObject park Implement                                      //
+    //****************************************************************************************************************//
+    static class NanoSecondsBlockSupport2 extends NanoSecondsBlockSupport {
+        NanoSecondsBlockSupport2(long nanoTime, Object blocker) {
+            super(nanoTime);
+            this.blockObject = blocker;
+        }
+
+        public final boolean block() {
+            parkNanos(blockObject, blockNanos);
+            return this.interrupted = Thread.interrupted();
+        }
+
+        public String toString() {
+            return "Implementation with method 'parkNanos(time,blockObject)'";
+        }
+    }
+
+    //****************************************************************************************************************//
+    //                                       MilliSeconds block Implement                                             //
+    //****************************************************************************************************************//
+    static class UtilMillsBlockSupport1 extends ThreadBlockSupport {
+        final long deadlineMillis;//nanoseconds or milliseconds
+
+        UtilMillsBlockSupport1(long deadline) {
+            this.deadlineMillis = deadline;
+            this.deadlineNanos = TimeUnit.MILLISECONDS.toNanos(deadlineMillis);//nanoseconds
+        }
+
+        public final long computeBlockTime() {
+            return this.blockNanos = deadlineNanos - System.nanoTime();
+        }
+
+        public boolean block() {
+            parkUntil(deadlineMillis);
+            return interrupted = Thread.interrupted();
+        }
+
+        public String toString() {
+            return "Implementation with method 'parkUntil(milliseconds)'";
+        }
+
+        public final void reset() {
+            throw new IllegalArgumentException("can't support absolute time reset");
+        }
+    }
+
+    //****************************************************************************************************************//
+    //                                       MilliSeconds block util Implement                                        //
+    //****************************************************************************************************************//
+    static class UtilMillsBlockSupport2 extends UtilMillsBlockSupport1 {
+        UtilMillsBlockSupport2(long deadline, Object blocker) {
+            super(deadline);
+            this.blockObject = blocker;
+        }
+
+        public boolean block() {
+            parkUntil(blockObject, deadlineMillis);
+            return this.interrupted = Thread.interrupted();
+        }
+
+        public String toString() {
+            return "Implementation with method 'parkUntil(milliseconds,blockObject)'";
+        }
+    }
+}
+
+
+
