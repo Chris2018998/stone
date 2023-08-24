0e6118f73df1d29cb29535b7a4006ca5684065d5
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2023-08-25 07:32:54 +0800

    Add files via upload

--- src/main/java/org/stone/shine/util/concurrent/synchronizer/SyncVisitTester.java
+++ src/main/java/org/stone/shine/util/concurrent/synchronizer/SyncVisitTester.java
@@ -21,7 +21,7 @@ import org.stone.tools.CommonUtil;
 public class SyncVisitTester {
     public static final SyncVisitTester COMMON_VISIT_TESTER = new SyncVisitTester();
 
-    public static final SyncVisitTester SHARE_VISIT_TESTER = new SyncVisitTester();
+    public static final SyncVisitTester SHARE_VISIT_TESTER = new ShareVisitTester();
 
     private SyncVisitTester() {
     }
