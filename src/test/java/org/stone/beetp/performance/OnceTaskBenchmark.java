cd39521f5f5bdc6b62c5fd5ef713d3f0974d641e
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2023-10-21 01:11:19 +0800

    Add files via upload

--- src/test/java/org/stone/beetp/performance/OnceTaskBenchmark.java
+++ src/test/java/org/stone/beetp/performance/OnceTaskBenchmark.java
@@ -1,6 +1,7 @@
 package org.stone.beetp.performance;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.BenchmarkParams;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
@@ -16,7 +17,7 @@ import java.util.concurrent.TimeUnit;
 @Threads(4)
 @State(Scope.Benchmark)
 @Warmup(iterations = 3, time = 1)
-@Measurement(iterations = 4, time = 1)
+@Measurement(iterations = 5, time = 1)
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class OnceTaskBenchmark {
@@ -24,15 +25,6 @@ public class OnceTaskBenchmark {
     private static ThreadPoolExecutor executor;
     private static CallTask callTask = new CallTask();
     private static HelloTask helloTask = new HelloTask();
-
-    static {
-        BeeTaskServiceConfig config = new BeeTaskServiceConfig();
-        config.setWorkInDaemon(true);
-        config.setInitWorkerSize(4);
-        config.setMaxWorkerSize(4);
-        taskService = new BeeTaskService(config);
-        executor = new ThreadPoolExecutor(4, 4, 15, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-    }
 
     public static void main(String[] args) throws Exception {
         Options opt = new OptionsBuilder()
@@ -42,16 +34,32 @@ public class OnceTaskBenchmark {
     }
 
     @Benchmark
-    @CompilerControl(CompilerControl.Mode.INLINE)
-    public static void testBeeTP() throws Exception {
+    public static void testBeeTaskExecutor() throws Exception {
         BeeTaskHandle handle = taskService.submit(helloTask);
         handle.get();
     }
 
     @Benchmark
-    @CompilerControl(CompilerControl.Mode.INLINE)
-    public static void testJDKExecutor() throws Exception {
+    public static void testThreadPoolExecutor() throws Exception {
         Future future = executor.submit(callTask);
         future.get();
+    }
+
+    @Setup(Level.Trial)
+    public void setup(BenchmarkParams params) {
+        executor = new ThreadPoolExecutor(4, 4, 15, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        executor.prestartAllCoreThreads();
+
+        BeeTaskServiceConfig config = new BeeTaskServiceConfig();
+        config.setInitWorkerSize(4);
+        config.setMaxWorkerSize(4);
+        config.setWorkerKeepAliveTime(TimeUnit.SECONDS.toMillis(15));
+        taskService = new BeeTaskService(config);
+    }
+
+    @TearDown
+    public void teardown() throws Exception {
+        executor.shutdownNow();
+        taskService.terminate(true);
     }
 }
