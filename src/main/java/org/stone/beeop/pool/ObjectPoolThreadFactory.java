fb357fb0ecd5551e97560e81245adc4fdb6ab860
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2023-12-02 14:33:23 +0800

    add default impl of thead factory[beeop]

---
+++ src/main/java/org/stone/beeop/pool/ObjectPoolThreadFactory.java
@@ -0,0 +1,46 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.beeop.pool;
+
+import org.stone.beeop.BeeObjectPoolThreadFactory;
+
+/**
+ * Default implementation of work thread factory
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public class ObjectPoolThreadFactory implements BeeObjectPoolThreadFactory {
+
+    /**
+     * create a thread to scan idle-timeout objects and remove them from pool
+     *
+     * @param runnable a runnable to be executed by new thread instance
+     * @return a created thread
+     */
+    public Thread createIdleScanThread(Runnable runnable) {
+        Thread thead = new Thread(runnable);
+        thead.setName("IdleScanThread");
+        return thead;
+    }
+
+    /**
+     * create a servant thread to search idle objects or create new objects,
+     * and transfer hold objects to waiters in pool
+     *
+     * @param runnable a runnable to be executed by new thread instance
+     * @return a created thread
+     */
+    public Thread createServantThread(Runnable runnable) {
+        Thread thead = new Thread(runnable);
+        thead.setName("ServantThread");
+        return thead;
+    }
+}
