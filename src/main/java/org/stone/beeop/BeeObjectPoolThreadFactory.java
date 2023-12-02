cca8ee58b13006db8f76d881ee1f71be9e90476c
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2023-12-02 11:41:09 +0800

    add pool work thread factory

---
+++ src/main/java/org/stone/beeop/BeeObjectPoolThreadFactory.java
@@ -0,0 +1,37 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under GNU Lesser General Public License v2.1.
+ */
+package org.stone.beeop;
+
+/**
+ * A Thread factory,which is used to create some work threads in object pool
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public interface BeeObjectPoolThreadFactory {
+
+    /**
+     * create thread to scan idle-timeout objects and remove them from pool
+     *
+     * @param runnable a runnable to be executed by new thread instance
+     * @return a created thread
+     */
+    Thread createIdleScanThread(Runnable runnable);
+
+    /**
+     * create a servant thread to search idle objects or create new objects,
+     * and transfer hold connections to waiters in pool
+     *
+     * @param runnable a runnable to be executed by new thread instance
+     * @return a created thread
+     */
+    Thread createServantThread(Runnable runnable);
+
+}
