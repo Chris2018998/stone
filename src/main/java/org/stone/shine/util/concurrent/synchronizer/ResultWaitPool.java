14bd564583de1df90b8a99ee3e4d7dc26afc13e9
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2023-12-06 19:45:32 +0800

    Add files via upload

--- src/main/java/org/stone/shine/util/concurrent/synchronizer/ResultWaitPool.java
+++ src/main/java/org/stone/shine/util/concurrent/synchronizer/ResultWaitPool.java
@@ -121,27 +121,27 @@ public final class ResultWaitPool extends ObjectWaitPool {
             }
 
             //4.2：try to block node thread
-            if (!(executeInd = node.receivedSignal())) {
-                if (isTimed) {
-                    long time = deadlineNanos - System.nanoTime();
-                    if (time <= 0L) {
-                        this.removeAndWakeupFirst(node);
-                        return validator.resultOnTimeout();
-                    }
-                    LockSupport.parkNanos(this, time);
-                } else {
-                    LockSupport.park(this);
-                }
-
-                //4.3：check whether interrupted
-                if (Thread.interrupted() && allowInterruption) {
+            //if (!(executeInd = node.receivedSignal())) {
+            if (isTimed) {
+                long time = deadlineNanos - System.nanoTime();
+                if (time <= 0L) {
                     this.removeAndWakeupFirst(node);
-                    throw new InterruptedException();
+                    return validator.resultOnTimeout();
                 }
-
-                //4.4：read node state
-                executeInd = node.receivedSignal();
+                LockSupport.parkNanos(this, time);
+            } else {
+                LockSupport.park(this);
             }
+
+            //4.3：check whether interrupted
+            if (Thread.interrupted() && allowInterruption) {
+                this.removeAndWakeupFirst(node);
+                throw new InterruptedException();
+            }
+
+            //4.4：read node state
+            executeInd = node.receivedSignal();
+            // }
         } while (true);
     }
 }
