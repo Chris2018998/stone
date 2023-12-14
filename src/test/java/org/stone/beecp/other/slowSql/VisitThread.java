c7117ad3f90859186c1389bba4c86d1d0ad50dd2
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2023-12-14 09:28:18 +0800

    add slow sql mock test case

---
+++ src/test/java/org/stone/beecp/other/slowSql/VisitThread.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright(C) Chris2018998
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.stone.beecp.other.slowSql;
+
+import org.stone.beecp.BeeDataSource;
+import org.stone.shine.util.concurrent.CountDownLatch;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+public class VisitThread extends Thread {
+    private long timePoint;
+    private BeeDataSource ds;
+    private CountDownLatch latch;
+
+
+    public VisitThread(BeeDataSource ds, CountDownLatch latch, long timePoint) {
+        this.ds = ds;
+        this.latch = latch;
+        this.timePoint = timePoint;
+    }
+
+    public void run() {
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            LockSupport.parkNanos(timePoint - System.nanoTime());//concurrent timePoint
+
+            con = ds.getConnection();
+            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(9));//delay 9seconds to execute sql
+            ps = con.prepareStatement("select * from dummyTable");
+            ps.execute();
+            latch.countDown();
+        } catch (Exception e) {
+        } finally {
+            if (ps != null) try {
+                ps.close();
+            } catch (Exception e) {
+            }
+            if (con != null) try {
+                con.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+}
