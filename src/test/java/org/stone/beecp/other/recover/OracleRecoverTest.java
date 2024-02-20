8f381a98de154975c854aabda4e9aa11275da6fc
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-02-20 14:27:59 +0800

    add recover tests on real database

---
+++ src/test/java/org/stone/beecp/other/recover/OracleRecoverTest.java
@@ -0,0 +1,32 @@
+package org.stone.beecp.other.recover;
+
+
+import org.stone.beecp.BeeDataSource;
+
+/**
+ * Jdk:Java8
+ * db:oracle12g
+ * driver:ojdbc7-12.1.0.2.jar
+ * pool:beecp-3.5.1/stone-1.2.7
+ * <p>
+ * test Result: Passed
+ */
+
+public class OracleRecoverTest extends BaseRecoverTest {
+
+    public static void main(String[] args) throws Exception {
+        BeeDataSource dataSource = new BeeDataSource();
+        dataSource.setDefaultAutoCommit(false);
+        dataSource.setInitialSize(1);
+        dataSource.setMaxActive(1);
+        dataSource.setDefaultSchema("SYSTEM");
+        dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        dataSource.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        dataSource.setUsername("system");
+        dataSource.setPassword("root");
+
+        OracleRecoverTest test = new OracleRecoverTest();
+        test.testSchema(dataSource, "Bee", "ORACLE", "SYSTEM", "SYS");
+    }
+
+}
