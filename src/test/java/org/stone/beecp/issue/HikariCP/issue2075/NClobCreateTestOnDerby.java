959b32553a3626f862f4550fb7e3bf1e2056fb94
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-02-25 19:11:58 +0800

    add new test case file for issue from other pool

--- src/test/java/org/stone/beecp/issue/HikariCP/issue2075/NClobCreateTestOnDerby.java
+++ src/test/java/org/stone/beecp/issue/HikariCP/issue2075/NClobCreateTestOnDerby.java
@@ -1,1 +1,79 @@
+package org.stone.beecp.issue.HikariCP.issue2075;
 
+import org.stone.beecp.BeeDataSource;
+import org.stone.beecp.BeeDataSourceConfig;
+import org.stone.beecp.pool.ProxyConnectionBase;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 1)Test Env:
+ * Java: Java-1.8.0_65-b17
+ * JDBC-Pool: stone-1.2.8
+ * DB: derby-10.14.2.0.jar(engine)
+ * Driver: derbytools-10.14.2.0.jar
+ * <dependency>
+ * <groupId>org.apache.derby</groupId>
+ * <artifactId>derby</artifactId>
+ * <version>10.17.1.0</version>
+ * <scope>test</scope>
+ * </dependency>
+ * <p>
+ * 2)Test Machines
+ * Server: Os:Win7_64,CPU:2.8Hz*2(PG80),Memory(8G)
+ * Client: Os:Win7_64,CPU:2.8Hz*2(PG80),Memory(8G)
+ * <p>
+ * 3)Test info
+ * Chris Liao(Stone project owner)
+ * Test Date: 2024/02/25 in China
+ * Test Result:Passed
+ */
+
+public class NClobCreateTestOnDerby {
+
+    public static void main(String[] args) throws Exception {
+        BeeDataSourceConfig config = new BeeDataSourceConfig();
+        config.setDefaultAutoCommit(false);
+        config.setInitialSize(1);
+        config.setMaxActive(1);
+        config.setUsername("test");
+        config.setPassword("test");
+        config.setDriverClassName("org.apache.derby.jdbc.EmbeddedDriver");
+        config.setJdbcUrl("jdbc:derby:testDB;create=true");
+        BeeDataSource ds = new BeeDataSource(config);
+
+        Field rawField = ProxyConnectionBase.class.getDeclaredField("raw");
+        rawField.setAccessible(true);
+
+        //test desc: test connection is whether broken when failed to create NClob
+        Connection con1 = null;
+        Object rawCon1 = null;
+
+        try {
+            con1 = ds.getConnection();
+            rawCon1 = rawField.get(con1);
+            con1.createNClob();//SQLFeatureNotSupportedException will thrown,because this feature not implemented in derby
+        } catch (SQLException e) {//test failed on HikariCP,because of a  ERROR 0A000(hardcode in source code)
+            e.printStackTrace();
+        } finally {
+            if (con1 != null) con1.close();
+        }
+
+        Connection con2 = null;
+        Object rawCon2 = null;
+        try {
+            con2 = ds.getConnection();
+            rawCon2 = rawField.get(con2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con2 != null) con2.close();
+        }
+
+        if (rawCon1 != rawCon2) throw new SQLException("Connection has broken");
+        System.out.println("Test case passed for creating NClob on derby");
+    }
+
+}
