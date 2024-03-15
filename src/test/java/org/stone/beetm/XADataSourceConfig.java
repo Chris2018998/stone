13886cf9ddff408efdacd2b775a75e18f54111c3
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-03-15 18:05:33 +0800

    Add files via upload

---
+++ src/test/java/org/stone/beetm/XADataSourceConfig.java
@@ -0,0 +1,23 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under Apache License v2.0.
+ */
+package org.stone.beetm;
+
+import org.stone.beecp.BeeDataSourceConfig;
+
+/**
+ * XA DataSource Configuration
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public class XADataSourceConfig extends BeeDataSourceConfig {
+
+    //**@todo
+}
