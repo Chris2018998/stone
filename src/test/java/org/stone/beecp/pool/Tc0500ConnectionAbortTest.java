7cc2a95acac2e91ba94d406fc670f29614bb2f4a
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-07-05 00:16:29 +0800

    remove some duplicated test files

--- src/test/java/org/stone/beecp/pool/Tc0500ConnectionAbortTest.java
+++ src/test/java/org/stone/beecp/pool/Tc0500ConnectionAbortTest.java
@@ -1,16 +1,12 @@
 package org.stone.beecp.pool;
 
 import junit.framework.TestCase;
-import org.junit.Assert;
-import org.stone.beecp.BeeConnectionPoolMonitorVo;
 import org.stone.beecp.BeeDataSource;
 import org.stone.beecp.BeeDataSourceConfig;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.stone.beecp.config.DsConfigFactory.createDefault;
-import static org.stone.beecp.pool.ConnectionPoolStatics.oclose;
 
 public class Tc0500ConnectionAbortTest extends TestCase {
     private BeeDataSource ds;
@@ -26,19 +22,8 @@ public class Tc0500ConnectionAbortTest extends TestCase {
     }
 
     public void testConnectionAbort() throws SQLException {
-        Connection con = null;
-        try {
-            BeeConnectionPoolMonitorVo vo = ds.getPoolMonitorVo();
-            Assert.assertEquals(4, vo.getIdleSize());
-            con = ds.getConnection();
-            con.abort(null);
 
-            vo = ds.getPoolMonitorVo();
-            Assert.assertEquals(3, vo.getIdleSize());
-        } finally {
-            if (con != null)
-                oclose(con);
-        }
+
     }
 }
 
