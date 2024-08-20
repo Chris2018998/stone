28f2443b4250d5bccec51e30f89271faf20c4b27
Chris2018998 <Chris2018998@tom.com>
2024-08-20 23:03:38 +0800

    add interface implementation class files for test

---
+++ src/test/java/org/stone/beeop/objects/JavaBookMethodFilter.java
@@ -0,0 +1,25 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under Apache License v2.0
+ */
+package org.stone.beeop.objects;
+
+import org.stone.beeop.BeeObjectMethodFilter;
+
+/**
+ * Java Method Filter
+ *
+ * @author Chris Liao
+ */
+
+public class JavaBookMethodFilter implements BeeObjectMethodFilter {
+
+    public void doFilter(Object key, String methodName, Class<?>[] paramTypes, Object[] paramValues) throws Exception {
+
+    }
+}
---
+++ src/test/java/org/stone/beeop/objects/JavaBookPredicate.java
@@ -0,0 +1,25 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under Apache License v2.0
+ */
+package org.stone.beeop.objects;
+
+import org.stone.beeop.BeeObjectPredicate;
+
+/**
+ * Java Book Predicate
+ *
+ * @author Chris Liao
+ */
+
+public class JavaBookPredicate implements BeeObjectPredicate {
+
+    public String evictTest(Exception e) {
+        return null;
+    }
+}
