818f0c0e8f7d1d1d74f96249c70a981671dc1f11
Chris2018998 <Chris2018998@tom.com>
2025-01-02 19:32:19 +0800

    remove final keyword

--- src/main/java/org/stone/beeop/BeeObjectSource.java
+++ src/main/java/org/stone/beeop/BeeObjectSource.java
@@ -29,7 +29,7 @@ import static org.stone.tools.BeanUtil.createClassInstance;
  * @author Chris Liao
  * @version 1.0
  */
-public final class BeeObjectSource extends BeeObjectSourceConfig {
+public class BeeObjectSource extends BeeObjectSourceConfig {
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
     private final ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
     private long maxWaitNanos = SECONDS.toNanos(8L);//default vale equals same item in config
