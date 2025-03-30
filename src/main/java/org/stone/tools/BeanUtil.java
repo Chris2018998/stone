db57a7615627c79014f0335229fe1e8ca38dd34b
Chris2018998 <Chris2018998@tom.com>
2025-03-30 13:04:28 +0800

    add  new util methods

--- src/main/java/org/stone/tools/BeanUtil.java
+++ src/main/java/org/stone/tools/BeanUtil.java
@@ -36,9 +36,10 @@ public class BeanUtil {
     public static final String Separator_MiddleLine = "-";
     //under-line:separator symbol in configuration properties name
     public static final String Separator_UnderLine = "_";
-
     //a SLF4 logger used in stone project
     public static final Logger CommonLog = LoggerFactory.getLogger(BeanUtil.class);
+    //Class loader
+    private static final ClassLoader classLoader = BeanUtil.class.getClassLoader();
 
     /**
      * set a field accessible under AccessController
@@ -225,6 +226,43 @@ public class BeanUtil {
     }
 
     /**
+     * attempt to load class with a class name
+     *
+     * @param className to be loaded
+     * @return a loaded class
+     * @throws ClassNotFoundException when class not found
+     */
+    public static Class<?> loadClass(String className) throws ClassNotFoundException {
+        return Class.forName(className, true, classLoader);
+    }
+
+    /**
+     * attempt to load class with a class name
+     *
+     * @param className to be loaded
+     * @return a loaded class
+     * @throws ClassNotFoundException when class not found
+     * @throws InstantiationException when class not found
+     * @throws IllegalAccessException when class not found
+     */
+    public static Object createClassInstance(String className) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        return Class.forName(className, true, classLoader).newInstance();
+    }
+
+    /**
+     * Create instance for a bean class
+     *
+     * @param beanClassName   is need be instantiated
+     * @param parentClass     is parent class for type check(it may be an interface should be implemented by bean class)
+     * @param objectClassType is a desc of bean class
+     * @return an instance of bean class
+     * @throws BeanException when create failed
+     */
+    public static Object createClassInstance(String beanClassName, Class<?> parentClass, String objectClassType) throws ClassNotFoundException, BeanException {
+        return createClassInstance(loadClass(beanClassName), parentClass != null ? new Class[]{parentClass} : null, objectClassType);
+    }
+
+    /**
      * Create instance for a bean class
      *
      * @param beanClass       is need be instantiated
@@ -337,7 +375,7 @@ public class BeanUtil {
         } else if (targetType == BigDecimal.class) {
             return new BigDecimal(text);
         } else if (targetType == Class.class) {
-            return Class.forName(text);
+            return loadClass(text);
         } else if (targetType.isArray()) {
             String[] textArray = text.split(Separator_Comma);
             int elementSize = textArray.length;
@@ -371,7 +409,7 @@ public class BeanUtil {
             }
             return collection;
         } else {
-            Object objInstance = Class.forName(text).newInstance();
+            Object objInstance = Class.forName(text, true, classLoader).newInstance();
             if (targetType.isInstance(objInstance)) return objInstance;
             throw new ClassCastException();
         }
