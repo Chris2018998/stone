62011fd36334dca2130fcd46921e9c6f0c3538fa
Chris2018998 <chris2018998@tom.com>
2025-10-26 16:26:39 +0800

    update copyright info

--- src/main/java/org/stone/beecp/BeeMethodExecutionListenerFactory.java
+++ src/main/java/org/stone/beecp/BeeMethodExecutionListenerFactory.java
@@ -1,17 +1,11 @@
 /*
- * Copyright Chris2018998
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright(C) Chris2018998,All rights reserved.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Project owner contact:Chris2018998@tom.com.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Project Licensed under Apache License v2.0.
  */
 package org.stone.beecp;
 
