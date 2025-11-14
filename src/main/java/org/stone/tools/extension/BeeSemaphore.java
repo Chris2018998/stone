be2bf79cbfba5e6450b5238218614042564b7c5f
Chris2018998 <chris2018998@tom.com>
2025-11-14 22:46:52 +0800

    Update Semaphore Impl

--- src/main/java/org/stone/tools/extension/BeeSemaphore.java
+++ src/main/java/org/stone/tools/extension/BeeSemaphore.java
@@ -25,23 +25,9 @@ import static org.stone.tools.CommonUtil.SPIN_FOR_TIMEOUT_THRESHOLD;
  * @version 1.0
  */
 public final class BeeSemaphore implements BeeInterruptable {
-    // VarHandle mechanics
-    private static final VarHandle permitStateHandle;
 
-    static {
-        try {
-            permitStateHandle = MethodHandles.lookup().findVarHandle(BeeSemaphorePermit.class, "state", int.class);
-        } catch (ReflectiveOperationException e) {
-            throw new ExceptionInInitializerError(e);
-        }
-    }
-
-    //
-    private final boolean unFair;
-    //Permits Array
-    private final BeeSemaphorePermit[] permits;
-    //Customization wait queue
-    private final BeeTransferQueue waitQueue;
+    //synchronizer
+    private final SemaphoreSynchronizer synchronizer;
 
     /**
      * Construct a semaphore with permit size.
@@ -59,12 +45,7 @@ public final class BeeSemaphore implements BeeInterruptable {
      * @param fair is true that fair mode
      */
     public BeeSemaphore(int size, boolean fair) {
-        this.unFair = !fair;
-        this.waitQueue = new BeeTransferQueue();
-        this.permits = new BeeSemaphorePermit[size];
-        for (int i = 0; i < size; i++) {
-            permits[i] = new BeeSemaphorePermit();
-        }
+        this.synchronizer = fair ? new FairSemaphore(size) : new NonFairSemaphore(size);
     }
 
     /**
@@ -75,65 +56,7 @@ public final class BeeSemaphore implements BeeInterruptable {
      * @throws InterruptedException when interruption occurred during waiting for a released permit
      */
     public BeeSemaphorePermit tryAcquire(long deadlineNanos, BeeTransferQueueNode node) throws InterruptedException {
-        if (unFair || waitQueue.existWaiters()) {
-            //1: search an idle permit from permit array
-            for (BeeSemaphorePermit permit : permits) {
-                if (permit.state == 0 && permitStateHandle.compareAndSet(permit, 0, 1)) {
-                    return permit;
-                }
-            }
-        }
-
-        //2: Offer node to wait queue
-        node.item = null;//set to null to accept a transferred value
-        waitQueue.offer(node);
-        Thread thread = node.thread;
-
-        //3:self-spin
-        do {
-            //3.1: read value which maybe filled by a releaser
-            Object value = node.item;//(value maybe an exception)
-            if (value instanceof BeeSemaphorePermit permit) {
-                if (permitStateHandle.compareAndSet(permit, 0, 1)) {
-                    this.waitQueue.remove(node);
-                    return permit;
-                }
-            }
-
-            //3.2 block thread via park
-            long remainTime = deadlineNanos - System.nanoTime();
-            if (remainTime > SPIN_FOR_TIMEOUT_THRESHOLD) {//park node thread
-                if (value != null) node.item = null;//set value to null to be filled by a releaser
-                LockSupport.parkNanos(remainTime);
-                if (thread.isInterrupted() && Thread.interrupted()) {//handle interruption
-                    this.waitQueue.remove(node);//remove first.
-
-                    value = node.item;
-                    if (value instanceof BeeSemaphorePermit permit) {
-                        if (permitStateHandle.compareAndSet(permit, 0, 1)) {
-                            release(permit);
-                        }
-                    }
-                    throw new InterruptedException();
-                }
-            } else if (remainTime > 0L) {//remain time is too short then use Thread.onSpinWait()
-                if (value != null) node.item = null;//set to null for be filled
-                Thread.onSpinWait();//spin wait
-            } else {//timeout
-                this.waitQueue.remove(node);//remove node at first.
-                if (value != null) return null;//if value read out at step3.1,so can't be filled by releaser
-
-                value = node.item;//read value
-                if (value instanceof BeeSemaphorePermit permit) {//OK,I got a released permit,so cas it
-                    if (permitStateHandle.compareAndSet(permit, 0, 1)) {
-                        return permit;
-                    }
-                }
-
-                //result for timeout
-                return null;
-            }
-        } while (true);
+        return this.synchronizer.tryAcquire(deadlineNanos, node);
     }
 
     /**
@@ -142,18 +65,165 @@ public final class BeeSemaphore implements BeeInterruptable {
      * @param permit to recycled
      */
     public void release(BeeSemaphorePermit permit) {
-        permitStateHandle.setVolatile(permit, 0);//set to idle state
-        waitQueue.tryTransfer(permit);
+        this.synchronizer.release(permit);
     }
 
-    //****************************************************************************************************************//
-    //                                        Queue operation of waiting threads                                      //
-    //****************************************************************************************************************//
     public List<Thread> getQueuedThreads() {
-        return waitQueue.getQueuedThreads();
+        return synchronizer.getQueuedThreads();
     }
 
     public List<Thread> interruptQueuedWaitThreads() {
-        return waitQueue.interruptQueuedWaitThreads();
+        return synchronizer.interruptQueuedWaitThreads();
+    }
+
+    //****************************************************************************************************************//
+    //                                        Synchronizer classes                                                    //
+    //****************************************************************************************************************//
+    private static class SemaphoreSynchronizer {
+        // VarHandle mechanics
+        protected static final VarHandle permitStateHandle;
+
+        static {
+            try {
+                permitStateHandle = MethodHandles.lookup().findVarHandle(BeeSemaphorePermit.class, "state", int.class);
+            } catch (ReflectiveOperationException e) {
+                throw new ExceptionInInitializerError(e);
+            }
+        }
+
+        //Permits Array
+        protected final BeeSemaphorePermit[] permits;
+        //Customization wait queue
+        protected final BeeTransferQueue waitQueue;
+
+        SemaphoreSynchronizer(int size) {
+            this.waitQueue = new BeeTransferQueue();
+            this.permits = new BeeSemaphorePermit[size];
+            for (int i = 0; i < size; i++) {
+                permits[i] = new BeeSemaphorePermit();
+            }
+        }
+
+        public List<Thread> getQueuedThreads() {
+            return waitQueue.getQueuedThreads();
+        }
+
+        public List<Thread> interruptQueuedWaitThreads() {
+            return waitQueue.interruptQueuedWaitThreads();
+        }
+
+
+        protected boolean allowTrySearch() {
+            return true;
+        }
+
+        protected boolean hold(BeeSemaphorePermit permit) {
+            return permit.state == 0 && permitStateHandle.compareAndSet(permit, 0, 1);
+        }
+
+        protected void release(BeeSemaphorePermit permit) {
+            permitStateHandle.setVolatile(permit, 0);//set to idle state
+            waitQueue.tryTransfer(permit);
+        }
+
+
+        /**
+         * Attempt to acquire a permit.
+         *
+         * @param deadlineNanos is a deadline time value
+         * @return acquired permit or null when timeout
+         * @throws InterruptedException when interruption occurred during waiting for a released permit
+         */
+        public BeeSemaphorePermit tryAcquire(long deadlineNanos, BeeTransferQueueNode node) throws InterruptedException {
+            if (allowTrySearch()) {
+                //1: search an idle permit from permit array
+                for (BeeSemaphorePermit permit : permits) {
+                    if (permit.state == 0 && permitStateHandle.compareAndSet(permit, 0, 1)) {
+                        return permit;
+                    }
+                }
+            }
+
+            //2: Offer node to wait queue
+            node.item = null;//set to null to accept a transferred value
+            waitQueue.offer(node);
+            Thread thread = node.thread;
+
+            //3:self-spin
+            do {
+                //3.1: read value which maybe filled by a releaser
+                Object value = node.item;//(value maybe an exception)
+                if (value instanceof BeeSemaphorePermit permit) {
+                    if (hold(permit)) {
+                        this.waitQueue.remove(node);
+                        return permit;
+                    }
+                }
+
+                //3.2 block thread via park
+                long remainTime = deadlineNanos - System.nanoTime();
+                if (remainTime > SPIN_FOR_TIMEOUT_THRESHOLD) {//park node thread
+                    if (value != null) node.item = null;//set value to null to be filled by a releaser
+                    LockSupport.parkNanos(remainTime);
+                    if (thread.isInterrupted() && Thread.interrupted()) {//handle interruption
+                        this.waitQueue.remove(node);//remove first.
+
+                        value = node.item;
+                        if (value instanceof BeeSemaphorePermit permit) {
+                            if (permitStateHandle.compareAndSet(permit, 0, 1)) {
+                                release(permit);
+                            }
+                        }
+                        throw new InterruptedException();
+                    }
+                } else if (remainTime > 0L) {//remain time is too short then use Thread.onSpinWait()
+                    if (value != null) node.item = null;//set to null for be filled
+                    Thread.onSpinWait();//spin wait
+                } else {//timeout
+                    this.waitQueue.remove(node);//remove node at first.
+                    if (value != null) return null;//if value read out at step3.1,so can't be filled by releaser
+
+                    value = node.item;//read value
+                    if (value instanceof BeeSemaphorePermit permit) {//OK,I got a released permit,so cas it
+                        if (permitStateHandle.compareAndSet(permit, 0, 1)) {
+                            return permit;
+                        }
+                    }
+
+                    //result for timeout
+                    return null;
+                }
+            } while (true);
+        }
+
+    }
+
+    private static class FairSemaphore extends SemaphoreSynchronizer {//Fair Implementation
+
+        FairSemaphore(int size) {
+            super(size);
+        }
+
+        protected boolean allowTrySearch() {
+            return !waitQueue.existWaiters();
+        }
+
+        protected boolean hold(BeeSemaphorePermit permit) {
+            return true;
+        }
+
+        protected void release(BeeSemaphorePermit permit) {
+            if (!waitQueue.tryTransfer(permit)) {
+                permitStateHandle.setVolatile(permit, 0);//set to idle state
+            }
+        }
+    }
+
+    private static class NonFairSemaphore extends SemaphoreSynchronizer {//NonFair Implementation
+
+        NonFairSemaphore(int size) {
+            super(size);
+        }
+
     }
 }
